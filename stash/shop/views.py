a884ae84e32e2e77725b783afff649537e20fad6
Collins njau <collinsnjau39@gmail.com>
2018-02-09 00:04:08 +0300

    I have rendered the categories in the template

--- stash/shop/views.py
+++ stash/shop/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Category
 
 # Create your views here.
 def welcome(request):
-    return render(request,"index.html")
+    categories=Category.objects.all()
+    
+    return render(request,"index.html",{"categories":categories})
