8b3f30b61f618bdde1b2cffeaa70fe0dca9acd16
Collins njau <collinsnjau39@gmail.com>
2018-02-08 22:37:33 +0300

    I have added the test for the category instance

--- stash/shop/tests.py
+++ stash/shop/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
-
+from .models import Category
 # Create your tests here.
+
+#class to test category model
+class categoryTest(TestCase):
+    #set up method to be run before each test
+    def setUp(self):
+        self.category=Category(category_name="chocolate")
+
+    #test that it is an instance of Category model
+    def test_instance(self):
+        self.assertTrue(isinstance(self.category,Category))
+            
